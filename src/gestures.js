07ed3711dfd7bb4bb5d22d7525c7eaac1943ba08
sloweyinuit <37492460+sloweyyy@users.noreply.github.com>
2023-06-28 03:27:46 +0700

    .

--- src/gestures.js
+++ src/gestures.js
@@ -1,26 +1,26 @@
 const { GestureDescription, Finger, FingerCurl, FingerDirection } = window.fp;
 
 const icons = [
-  'thumbs_up', 'thumbs_down', 'rock', 'paper', 'paper', 'scissors', 'middle', 'fazol', 'ok', 'pinching',
-  'painLeft', 'top', 'show', 'yes', 'horn', 'rightHand'
+    'thumbs_up', 'thumbs_down', 'rock', 'paper', 'paper', 'scissors', 'middle', 'fazol', 'ok', 'pinching',
+    'painLeft', 'top', 'show', 'yes', 'horn', 'rightHand'
 ]
 
 const [
-  thumbsUpGesture,
-  thumbsDownGesture,
-  rockGesture,
-  paperGesture,
-  scissorsGesture,
-  middleGesture,
-  fazoLGesture,
-  okGesture,
-  pinchingGesture,
-  painLeftGesture,
-  topGesture,
-  showGesture,
-  yesGesture,
-  hornGesture,
-  rightHandGesture
+    thumbsUpGesture,
+    thumbsDownGesture,
+    rockGesture,
+    paperGesture,
+    scissorsGesture,
+    middleGesture,
+    fazoLGesture,
+    okGesture,
+    pinchingGesture,
+    painLeftGesture,
+    topGesture,
+    showGesture,
+    yesGesture,
+    hornGesture,
+    rightHandGesture
 ] = icons.map((icon) => new GestureDescription(icon))
 
 // -----------------------------------------------------------------------------
@@ -33,17 +33,17 @@ const [
 thumbsUpGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0);
 thumbsUpGesture.addDirection(Finger.Thumb, FingerDirection.VerticalUp, 1.0);
 thumbsUpGesture.addDirection(
-  Finger.Thumb,
-  FingerDirection.DiagonalUpLeft,
-  0.25
+    Finger.Thumb,
+    FingerDirection.DiagonalUpLeft,
+    0.25
 );
 
 // all other fingers:
 // - curled
 // - horizontal left or right
-for (let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
-  thumbsUpGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
-  thumbsUpGesture.addDirection(finger, FingerDirection.HorizontalLeft, 1.0);
+for (let finger of[Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
+    thumbsUpGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
+    thumbsUpGesture.addDirection(finger, FingerDirection.HorizontalLeft, 1.0);
 }
 
 // -----------------------------------------------------------------------------
@@ -56,17 +56,17 @@ for (let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
 thumbsDownGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0);
 thumbsUpGesture.addDirection(Finger.Thumb, FingerDirection.VerticalDown, 1.0);
 thumbsDownGesture.addDirection(
-  Finger.Thumb,
-  FingerDirection.DiagonalDownLeft,
-  0.7
+    Finger.Thumb,
+    FingerDirection.DiagonalDownLeft,
+    0.7
 );
 
 // all other fingers:
 // - curled
 // - horizontal left or right
-for (let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
-  thumbsDownGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
-  thumbsDownGesture.addDirection(finger, FingerDirection.HorizontalLeft, 1.0);
+for (let finger of[Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
+    thumbsDownGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
+    thumbsDownGesture.addDirection(finger, FingerDirection.HorizontalLeft, 1.0);
 }
 
 // -----------------------------------------------------------------------------
@@ -78,22 +78,22 @@ for (let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
 rockGesture.addCurl(Finger.Thumb, FingerCurl.HalfCurl, 1.0);
 
 // all other fingers: curled
-for (let finger of [Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
-  rockGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
+for (let finger of[Finger.Index, Finger.Middle, Finger.Ring, Finger.Pinky]) {
+    rockGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
 }
 
 // -----------------------------------------------------------------------------
 // Paper
 // -----------------------------------------------------------------------------
 
-for (let finger of [
-  Finger.Thumb,
-  Finger.Index,
-  Finger.Middle,
-  Finger.Ring,
-  Finger.Pinky,
-]) {
-  paperGesture.addCurl(finger, FingerCurl.NoCurl, 0.9);
+for (let finger of[
+        Finger.Thumb,
+        Finger.Index,
+        Finger.Middle,
+        Finger.Ring,
+        Finger.Pinky,
+    ]) {
+    paperGesture.addCurl(finger, FingerCurl.NoCurl, 0.9);
 }
 
 // Thumb
@@ -117,39 +117,39 @@ paperGesture.addDirection(Finger.Pinky, FingerDirection.DiagonalUpRight, 0.9);
 // thumb:
 rightHandGesture.addCurl(Finger.Thumb, FingerCurl.NoCurl, 1.0);
 rightHandGesture.addDirection(
-  Finger.Thumb,
-  FingerDirection.DiagonalUpRight,
-  0.9
+    Finger.Thumb,
+    FingerDirection.DiagonalUpRight,
+    0.9
 );
 
 // index:
 rightHandGesture.addCurl(Finger.Index, FingerCurl.NoCurl, 0.9);
 rightHandGesture.addDirection(
-  Finger.Index,
-  FingerDirection.HorizontalRight,
-  0.9
+    Finger.Index,
+    FingerDirection.HorizontalRight,
+    0.9
 );
 
 // Middle:
 rightHandGesture.addCurl(Finger.Middle, FingerCurl.NoCurl, 0.9);
 rightHandGesture.addDirection(
-  Finger.Middle,
-  FingerDirection.HorizontalRight,
-  0.9
+    Finger.Middle,
+    FingerDirection.HorizontalRight,
+    0.9
 );
 // Ring:
 rightHandGesture.addCurl(Finger.Ring, FingerCurl.NoCurl, 0.9);
 rightHandGesture.addDirection(
-  Finger.Ring,
-  FingerDirection.HorizontalRight,
-  0.9
+    Finger.Ring,
+    FingerDirection.HorizontalRight,
+    0.9
 );
 // Pinky:
 rightHandGesture.addCurl(Finger.Pinky, FingerCurl.NoCurl, 0.9);
 rightHandGesture.addDirection(
-  Finger.Pinky,
-  FingerDirection.HorizontalRight,
-  0.9
+    Finger.Pinky,
+    FingerDirection.HorizontalRight,
+    0.9
 );
 
 //------------------------------------------------------------------------------
@@ -234,8 +234,8 @@ pinchingGesture.addCurl(Finger.Index, FingerCurl.HalfCurl, 1.0);
 pinchingGesture.addDirection(Finger.Index, FingerDirection.DiagonalUpLeft, 0.8);
 
 // all other fingers:
-for (let finger of [Finger.Middle, Finger.Ring, Finger.Pinky]) {
-  pinchingGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
+for (let finger of[Finger.Middle, Finger.Ring, Finger.Pinky]) {
+    pinchingGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
 }
 
 //------------------------------------------------------------------------------
@@ -249,9 +249,9 @@ painLeftGesture.addCurl(Finger.Index, FingerCurl.HalfCurl, 1.0);
 painLeftGesture.addDirection(Finger.Index, FingerDirection.HorizontalLeft, 0.8);
 
 // all other fingers:
-for (let finger of [Finger.Middle, Finger.Ring, Finger.Pinky]) {
-  painLeftGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
-  painLeftGesture.addDirection(finger, FingerDirection.HorizontalLeft, 0.8);
+for (let finger of[Finger.Middle, Finger.Ring, Finger.Pinky]) {
+    painLeftGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
+    painLeftGesture.addDirection(finger, FingerDirection.HorizontalLeft, 0.8);
 }
 
 //------------------------------------------------------------------------------
@@ -265,8 +265,8 @@ topGesture.addCurl(Finger.Index, FingerCurl.NoCurl, 1.0);
 topGesture.addDirection(Finger.Index, FingerDirection.VerticalUp, 0.9);
 
 // all other fingers:
-for (let finger of [Finger.Middle, Finger.Ring, Finger.Pinky]) {
-  topGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
+for (let finger of[Finger.Middle, Finger.Ring, Finger.Pinky]) {
+    topGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
 }
 
 //------------------------------------------------------------------------------
@@ -279,9 +279,9 @@ showGesture.addDirection(Finger.Thumb, FingerDirection.DiagonalUpLeft, 0.25);
 // all other fingers:
 // - curled
 // - horizontal left or right
-for (let finger of [Finger.Index, Finger.Middle, Finger.Ring]) {
-  showGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
-  showGesture.addDirection(finger, FingerDirection.HorizontalLeft, 1.0);
+for (let finger of[Finger.Index, Finger.Middle, Finger.Ring]) {
+    showGesture.addCurl(finger, FingerCurl.FullCurl, 1.0);
+    showGesture.addDirection(finger, FingerDirection.HorizontalLeft, 1.0);
 }
 showGesture.addCurl(Finger.Pinky, FingerCurl.HalfCurl, 1.0);
 showGesture.addDirection(Finger.Pinky, FingerDirection.HorizontalLeft, 1.0);
@@ -325,21 +325,21 @@ hornGesture.addCurl(Finger.Ring, FingerCurl.FullCurl, 0.9);
 hornGesture.addCurl(Finger.Pinky, FingerCurl.NoCurl, 1.0);
 
 const gestures = [
-  thumbsUpGesture,
-  thumbsDownGesture,
-  rockGesture,
-  paperGesture,
-  scissorsGesture,
-  middleGesture,
-  okGesture,
-  fazoLGesture,
-  pinchingGesture,
-  painLeftGesture,
-  topGesture,
-  showGesture,
-  yesGesture,
-  hornGesture,
-  rightHandGesture,
+    thumbsUpGesture,
+    thumbsDownGesture,
+    rockGesture,
+    paperGesture,
+    scissorsGesture,
+    middleGesture,
+    okGesture,
+    fazoLGesture,
+    pinchingGesture,
+    painLeftGesture,
+    topGesture,
+    showGesture,
+    yesGesture,
+    hornGesture,
+    rightHandGesture,
 ];
 
 export { gestures };
